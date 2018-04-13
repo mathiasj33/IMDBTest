bf2b11bc5687ce22f31c2253083d6486c8295eb9
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2018-04-13 21:38:50 +0200

    Removed everything unrelated to keras

--- train_and_evaluate_keras.py
+++ train_and_evaluate_keras.py
@@ -1,6 +1,7 @@
 from data_loader import DataLoader, DataType
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
+from keras.regularizers import l2
 import numpy as np
 
 dl = DataLoader()
@@ -11,12 +12,13 @@ test_x = np.squeeze(np.array([v for v,l in test]))
 test_y = np.array([l for v,l in test])
 
 model = Sequential()
-model.add(Dense(units=30, activation='relu', input_dim=500))
-model.add(Dense(units=1, activation='sigmoid'))
+model.add(Dense(units=30, activation='relu', input_dim=500, kernel_regularizer=l2(0.01)))
+model.add(Dropout(0.5))
+model.add(Dense(units=1, activation='sigmoid', kernel_regularizer=l2(0.01)))
 model.compile(loss='binary_crossentropy',
-              optimizer='sgd',
+              optimizer='adagrad',
               metrics=['accuracy'])
 
-model.fit(train_x, train_y, epochs=30, batch_size=10)
-loss_and_metrics = model.evaluate(train_x, train_y, batch_size=128)
+model.fit(train_x, train_y, epochs=100, batch_size=10)
+loss_and_metrics = model.evaluate(test_x, test_y, batch_size=128)
 print(loss_and_metrics)
